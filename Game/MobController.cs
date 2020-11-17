fac34c1a5ba65e83ac424ed1f99a3f03ac8018d5
Tanner Evins <tanner@tannerevins.com>
2020-11-17 12:12:34 -0600

    Add attack behavior to AI

--- Game/MobController.cs
+++ Game/MobController.cs
@@ -1,11 +1,18 @@
-﻿using Nez;
+﻿using Microsoft.Xna.Framework;
+using Nez;
 
 namespace Game
 {
     class MobController : ControllerComponent, IUpdatable
     {
-        public override float XAxis => _dir;
+        public int LinecastDistance = 150;
+        public float AttackTimeout = 1f;
+
+        public override float XAxis => _attackTimeoutActive ? 0 : _dir;
         float _dir = 1f;
+        public override bool AttackPressed => _attack;
+        bool _attack = false;
+        bool _attackTimeoutActive = false;
 
         CollisionComponent _collision;
 
@@ -17,10 +24,22 @@ namespace Game
 
         public void Update()
         {
+            _attack = false;
+
             if (_collision.Collision.Right || _collision.Collision.Left)
                 _dir = -_dir;
             if (Entity.Position.X <= 0)
                 _dir = 1f;
+
+            var hit = Physics.Linecast(
+                Entity.Position,
+                Entity.Position + new Vector2(LinecastDistance * _dir, 0),
+                Layer.HurtBox);
+            if (hit.Collider != null && !_attackTimeoutActive)
+            {
+                _attack = _attackTimeoutActive = true;
+                Core.Schedule(AttackTimeout, timer => _attackTimeoutActive = false);
+            }
         }
     }
 }
