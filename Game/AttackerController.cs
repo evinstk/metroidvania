b45410a1650a5b6f17c1d5ba4565c96c208ad29c
Tanner Evins <tanner@tannerevins.com>
2020-11-19 16:32:54 -0600

    Convert attack controller to behavior tree

--- Game/AttackerController.cs
+++ Game/AttackerController.cs
@@ -1,31 +1,48 @@
 ﻿using Microsoft.Xna.Framework;
 using Nez;
+using Nez.AI.BehaviorTrees;
 
 namespace Game
 {
     class AttackerController : ControllerComponent, IUpdatable
     {
         public int LinecastDistance = 150;
-        public float AttackTimeout = 1f;
+        public float AttackTimeout = 0.8f;
 
-        public override float XAxis => _attackTimeoutActive ? 0 : _dir;
+        public override float XAxis => _moving ? _dir : 0;
         float _dir = 1f;
         public override bool AttackPressed => _attack;
         bool _attack = false;
-        bool _attackTimeoutActive = false;
+        bool _moving = false;
 
         CollisionComponent _collision;
+
+        BehaviorTree<AttackerController> _tree;
 
         public override void OnAddedToEntity()
         {
             _collision = Entity.GetComponent<CollisionComponent>();
             Insist.IsNotNull(_collision);
+
+            var builder = BehaviorTreeBuilder<AttackerController>.Begin(this);
+
+            builder.Sequence()
+                .Action(m => m.FindTarget())
+                .WaitAction(AttackTimeout)
+                .EndComposite();
+
+            _tree = builder.Build();
         }
 
         public void Update()
         {
             _attack = false;
+            _tree.Tick();
+        }
 
+        TaskStatus FindTarget()
+        {
+            _moving = true;
             if (_collision.Collision.Right || _collision.Collision.Left)
                 _dir = -_dir;
             if (Entity.Position.X <= 0)
@@ -35,11 +52,14 @@ namespace Game
                 Entity.Position,
                 Entity.Position + new Vector2(LinecastDistance * _dir, 0),
                 1 << Layer.HurtBox);
-            if (hit.Collider != null && !_attackTimeoutActive)
+            if (hit.Collider != null)
             {
-                _attack = _attackTimeoutActive = true;
-                Core.Schedule(AttackTimeout, timer => _attackTimeoutActive = false);
+                _attack = true;
+                _moving = false;
+                return TaskStatus.Success;
             }
+
+            return TaskStatus.Running;
         }
     }
 }
