4b6c801d39e5a25e2a803b4f23f47f212ce3c0f5
Tanner Evins <tanner@tannerevins.com>
2020-11-23 17:53:17 -0600

    Reset scene on start button down

--- Game/GameManager.cs
+++ Game/GameManager.cs
@@ -7,7 +7,7 @@ namespace Game
     {
         public void Update()
         {
-            if (Input.IsKeyDown(Keys.Delete))
+            if (Input.IsKeyDown(Keys.Delete) || Input.GamePads[0].IsButtonDown(Buttons.Start))
             {
                 var scene = Core.Scene as MainScene;
                 Game.Transition(scene.MapSrc, scene.Spawn, scene.StartingHealth);
