0488ca584ae3bb0719e52f53e742a4c05dd83992
Tanner Evins <tanner@tannerevins.com>
2020-12-05 09:06:23 -0600

    Add `TriggerListener` component

---
+++ Game/TriggerListener.cs
@@ -0,0 +1,21 @@
+﻿using Nez;
+using System;
+
+namespace Game
+{
+    class TriggerListener : Component, ITriggerListener
+    {
+        public Action TriggerEnter { get;  set; }
+        public Action TriggerExit { get; set; }
+
+        public void OnTriggerEnter(Collider other, Collider local)
+        {
+            TriggerEnter?.Invoke();
+        }
+
+        public void OnTriggerExit(Collider other, Collider local)
+        {
+            TriggerExit?.Invoke();
+        }
+    }
+}
