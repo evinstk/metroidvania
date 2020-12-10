754c313ff7ea1d3c1a0f1ddad01797ebca55e2c3
Tanner Evins <tanner@tannerevins.com>
2020-12-09 19:06:45 -0600

    Add detailed tileset

---
+++ Game/Content/Maps/tileset.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tileset" tilewidth="32" tileheight="32" tilecount="64" columns="8">
+ <image source="../Textures/tileset.png" width="256" height="256"/>
+ <wangsets>
+  <wangset name="New Wang Set" tile="-1">
+   <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="7" wangid="0x1010102"/>
+   <wangtile tileid="15" wangid="0x1010201"/>
+   <wangtile tileid="22" wangid="0x1020102"/>
+   <wangtile tileid="23" wangid="0x1020101"/>
+   <wangtile tileid="30" wangid="0x2010201"/>
+   <wangtile tileid="31" wangid="0x2010101"/>
+   <wangtile tileid="38" wangid="0x2020201"/>
+   <wangtile tileid="39" wangid="0x1010202"/>
+   <wangtile tileid="46" wangid="0x1020202"/>
+   <wangtile tileid="47" wangid="0x1020201"/>
+   <wangtile tileid="54" wangid="0x2010202"/>
+   <wangtile tileid="55" wangid="0x2020101"/>
+   <wangtile tileid="62" wangid="0x2020102"/>
+   <wangtile tileid="63" wangid="0x2010102"/>
+  </wangset>
+  <wangset name="platform" tile="-1">
+   <wangedgecolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangedgecolor name="" color="#0000ff" tile="-1" probability="1"/>
+   <wangtile tileid="1" wangid="0x2030103"/>
+   <wangtile tileid="2" wangid="0x1030203"/>
+  </wangset>
+ </wangsets>
+</tileset>
