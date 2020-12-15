3a684435980653c624d9d73a1793815863204b84
Tanner Evins <tanner@tannerevins.com>
2020-12-15 16:33:10 -0600

    Load world for editor

--- Game/EditorScene.cs
+++ Game/EditorScene.cs
@@ -1,4 +1,6 @@
 ﻿using Nez;
+using Game.Tiled;
+using System.IO;
 
 namespace Game
 {
@@ -8,6 +10,17 @@ namespace Game
         {
             SetDesignResolution(MainScene.ResWidth, MainScene.ResHeight, SceneResolutionPolicy.ShowAllPixelPerfect);
             Screen.SetSize(MainScene.ScreenWidth, MainScene.ScreenHeight);
+
+            var world = Json.ReadJson<World>("Content/Maps/world8.world");
+
+            foreach (var m in world.Maps)
+            {
+                var map = Content.LoadTiledMap($"Content/Maps/{m.FileName}");
+                var mapEntity = CreateEntity(Path.GetFileNameWithoutExtension(m.FileName));
+                mapEntity
+                    .SetPosition(m.X, m.Y)
+                    .AddComponent(new TiledMapRenderer(map));
+            }
         }
     }
 }
---
+++ Game/World.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Game.Tiled
+{
+    class World
+    {
+        public List<Map> Maps;
+    }
+
+    class Map
+    {
+        public string FileName;
+        public int Height;
+        public int Width;
+        public int X;
+        public int Y;
+    }
+}
