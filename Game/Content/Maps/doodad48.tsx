b95c7a747e548ef0c54efe8697d6a833ba92146a
Tanner Evins <tanner@tannerevins.com>
2020-12-24 08:18:35 -0600

    Add doodad48

---
+++ Game/Content/Maps/doodad48.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="doodad48" tilewidth="48" tileheight="48" tilecount="100" columns="10">
+ <image source="../Textures/doodad48.png" width="512" height="512"/>
+</tileset>
