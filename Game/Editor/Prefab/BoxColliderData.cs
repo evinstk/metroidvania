331a9d1564bb16513eb828384ba5e317a53eb719
Tanner Evins <tanner@tannerevins.com>
2021-01-02 09:40:00 -0600

    Add box collider and controller prefab components

---
+++ Game/Editor/Prefab/BoxColliderData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+
+namespace Game.Editor.Prefab
+{
+    class BoxColliderData : PrefabComponent
+    {
+        public Vector2 Size;
+
+        public override void AddToEntity(Entity entity)
+        {
+            var collider = entity.AddComponent<BoxCollider>();
+            collider.SetSize(Size.X, Size.Y);
+        }
+    }
+}
---
+++ Game/Editor/Prefab/ControllerData.cs
@@ -0,0 +1,12 @@
+﻿using Nez;
+
+namespace Game.Editor.Prefab
+{
+    class PlayerControllerData : PrefabComponent
+    {
+        public override void AddToEntity(Entity entity)
+        {
+            entity.AddComponent<PlayerController>();
+        }
+    }
+}
