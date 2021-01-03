1c2620849d532da0540ff85f3640ffaf069984ca
Tanner Evins <tanner@tannerevins.com>
2021-01-03 15:12:01 -0600

    Add FollowCameraData

---
+++ Game/Editor/Prefab/FollowCameraData.cs
@@ -0,0 +1,13 @@
+﻿using Nez;
+
+namespace Game.Editor.Prefab
+{
+    class FollowCameraData : PrefabComponent
+    {
+        public override void AddToEntity(Entity entity)
+        {
+            var followCamera = entity.AddComponent(new FollowCamera(entity, FollowCamera.CameraStyle.CameraWindow));
+            followCamera.FollowLerp = 1f;
+        }
+    }
+}
