27873d62b763b27a8d21de6051b1f57f2fcc3bd9
Tanner Evins <tanner@tannerevins.com>
2021-01-09 15:12:29 -0600

    Add checkpoint and script content

--- Game/Content/Scripts/room.lua
+++ Game/Content/Scripts/room.lua
@@ -1,1 +1,20 @@
-player = findEntity("Hero")
+player = findEntity("hero")
+lamp = findEntity("Special Lamp")
+area = findEntity("Some Area")
+practiceArea = findEntity("practiceArea")
+
+test_dialog = cutscene(player, function()
+    dialog("This is long text to make sure text wrap algorithm doesn't completely screw up the dialog box output.")
+    dialog("This text comes after input from the player.")
+end)
+
+trigger(
+    onEnter(player, area),
+    test_dialog)
+
+-- trigger(
+--     onEnter(player, practiceArea),
+--     cutscene(player, function()
+--         dialog("This text is now edited.")
+--     end)
+-- )
