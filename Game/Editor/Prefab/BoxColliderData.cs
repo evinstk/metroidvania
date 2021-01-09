f213626694419ccd4afab5705856cece2093999b
Tanner Evins <tanner@tannerevins.com>
2021-01-09 10:32:08 -0600

    Lift physics layer data to new class

--- Game/Editor/Prefab/BoxColliderData.cs
+++ Game/Editor/Prefab/BoxColliderData.cs
@@ -1,38 +1,20 @@
-﻿using ImGuiNET;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Nez;
-using Nez.ImGuiTools.TypeInspectors;
 
 namespace Game.Editor.Prefab
 {
     class BoxColliderData : DataComponent
     {
         public Vector2 Size;
-        //public int PhysicsLayer = -1;
-        [CustomInspector(typeof(PhysicsInspector))]
-        public class Physics
-        {
-            public int PhysicsLayer = -1;
-            public int CollidesWithLayers = -1;
-        }
-        public Physics PhysicsData = new Physics();
+        public PhysicsLayerData PhysicsLayer = new PhysicsLayerData { Name = "Physics Layer" };
+        public PhysicsLayerData CollidesWithLayers = new PhysicsLayerData { Name = "Collides With" };
 
         public override void AddToEntity(Entity entity)
         {
             var collider = entity.AddComponent<BoxCollider>();
             collider.SetSize(Size.X, Size.Y);
-            collider.PhysicsLayer = PhysicsData.PhysicsLayer;
-            collider.CollidesWithLayers = PhysicsData.CollidesWithLayers;
-        }
-    }
-
-    class PhysicsInspector : AbstractTypeInspector
-    {
-        public override void DrawMutable()
-        {
-            var physicsData = GetValue<BoxColliderData.Physics>();
-            ImGuiExt.DrawPhysicsLayerInput("Physics Layer", ref physicsData.PhysicsLayer);
-            ImGuiExt.DrawPhysicsLayerInput("Collides With", ref physicsData.CollidesWithLayers);
+            collider.PhysicsLayer = PhysicsLayer.Mask;
+            collider.CollidesWithLayers = CollidesWithLayers.Mask;
         }
     }
 }
---
+++ Game/Editor/Prefab/PhysicsLayerData.cs
@@ -0,0 +1,21 @@
+﻿using Nez;
+using Nez.ImGuiTools.TypeInspectors;
+
+namespace Game.Editor.Prefab
+{
+    [CustomInspector(typeof(PhysicsInspector))]
+    class PhysicsLayerData
+    {
+        public string Name = "Mask";
+        public int Mask = -1;
+    }
+
+    class PhysicsInspector : AbstractTypeInspector
+    {
+        public override void DrawMutable()
+        {
+            var physicsData = GetValue<PhysicsLayerData>();
+            ImGuiExt.DrawPhysicsLayerInput(physicsData.Name, ref physicsData.Mask);
+        }
+    }
+}
