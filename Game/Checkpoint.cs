7a576e028c8f1df464c47dd8d3d683b05f3e24a6
Tanner Evins <tanner@tannerevins.com>
2021-01-09 14:48:58 -0600

    Add checkpoint components and prefab

---
+++ Game/Checkpoint.cs
@@ -0,0 +1,40 @@
+﻿using Game.Editor;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Nez;
+
+namespace Game
+{
+    class Checkpoint : Component, IUpdatable
+    {
+        public Vector2 Size = new Vector2(32, 32);
+        public int PhysicsMask = -1;
+
+        VirtualButton _input;
+
+        RoomEntityComponent _roomEntity;
+
+        public override void OnAddedToEntity()
+        {
+            _input = new VirtualButton();
+            _input.Nodes.Add(new VirtualButton.KeyboardKey(Keys.S));
+            _input.Nodes.Add(new VirtualButton.GamePadButton(0, Buttons.DPadDown));
+
+            _roomEntity = Entity.GetComponentStrict<RoomEntityComponent>();
+        }
+
+        public void Update()
+        {
+            // this logic should probably belong on a component on the player entity
+            if (_input.IsPressed)
+            {
+                var rect = new RectangleF(
+                    Entity.Position - Size / 2,
+                    Size);
+                var collider = Physics.OverlapRectangle(rect, PhysicsMask);
+                if (collider != null)
+                    SaveSystem2.Save(_roomEntity.RoomEntityId, _roomEntity.RoomId);
+            }
+        }
+    }
+}
---
+++ Game/SaveSystem2.cs
@@ -0,0 +1,43 @@
+﻿using Nez.Persistence;
+using System.IO;
+
+namespace Game
+{
+    static class SaveSystem2
+    {
+        public class SaveFile
+        {
+            public string RoomId = "ICLOHYCSXGMCKXQRKOULNXNVVHJKNWYYFGSXGO";
+            public string CheckpointId = "LKSBKDXEHTMWQXJYMQATLDOVLNDCKPTTFGJZLI";
+        }
+
+        public static void Save(string roomId, string checkpointId)
+        {
+            var saveFile = new SaveFile
+            {
+                RoomId = roomId,
+                CheckpointId = checkpointId,
+            };
+            var serialized = Json.ToJson(saveFile, new JsonSettings
+            {
+                PrettyPrint = true,
+            });
+            File.WriteAllText(GameFile.SavePath, serialized);
+        }
+
+        public static SaveFile Load()
+        {
+            SaveFile data;
+            try
+            {
+                var serialized = File.ReadAllText(GameFile.SavePath);
+                data = Json.FromJson<SaveFile>(serialized);
+            }
+            catch (FileNotFoundException)
+            {
+                data = new SaveFile();
+            }
+            return data;
+        }
+    }
+}
