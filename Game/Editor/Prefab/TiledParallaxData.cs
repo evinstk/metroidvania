0a5e9bdd13d0afcee5dc76a36058801b993ad0d9
Tanner Evins <tanner@tannerevins.com>
2021-01-08 20:56:06 -0600

    Add TiledParallaxData

---
+++ Game/Editor/Prefab/TiledParallaxData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using System.IO;
+
+namespace Game.Editor.Prefab
+{
+    class TiledParallaxData : PrefabComponent
+    {
+        public SpriteTextureData TextureData = new SpriteTextureData();
+        public Vector2 ScrollScale = new Vector2(1, 0);
+        public int RenderLayer = 99;
+
+        public override void AddToEntity(Entity entity)
+        {
+            var backgroundEntity = entity.Scene.CreateEntity(Path.GetFileNameWithoutExtension(TextureData.TextureFile));
+            var renderer = backgroundEntity.AddComponent(new TiledSpriteRenderer(TextureData.Texture));
+            renderer.RenderLayer = RenderLayer;
+            var parallax = backgroundEntity.AddComponent<TiledParallaxComponent>();
+            parallax.ScrollScale = ScrollScale;
+            backgroundEntity.Parent = entity.Transform;
+            backgroundEntity.UpdateOrder = int.MaxValue;
+        }
+    }
+}
