b0c56e109179789ad780f722b662bfc72860e341
Tanner Evins <tanner@tannerevins.com>
2021-01-28 07:26:46 -0600

    Add StringValue

---
+++ Game/Editor/Scriptable/StringValue.cs
@@ -0,0 +1,22 @@
+﻿using Nez;
+using Nez.Persistence;
+
+namespace Game.Editor.Scriptable
+{
+    class StringValue : ScriptableObject, IValue<string>
+    {
+        [JsonInclude]
+        public string InitialValue { get; set; } = string.Empty;
+        [NotInspectable]
+        [JsonInclude]
+        public string RuntimeValue { get; set; } = string.Empty;
+
+        public override void OnStart()
+        {
+            RuntimeValue = InitialValue;
+        }
+    }
+
+    [CustomInspector(typeof(ReferenceInspector<StringValue>))]
+    class StringReference : Reference<StringValue> {}
+}
