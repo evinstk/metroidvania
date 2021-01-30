61c3afb5dd603ce7f78ef091e79da31074e20cbb
Tanner Evins <tanner@tannerevins.com>
2021-01-30 13:11:24 -0600

    Update content

--- Game/Content/Scripts/room.lua
+++ Game/Content/Scripts/room.lua
@@ -1,36 +1,36 @@
-local player = findEntity("hero")
-local lamp = findEntity("Special Lamp")
-local area = findEntity("Some Area")
-local practiceArea = findEntity("practiceArea")
+-- local player = findEntity("hero")
+-- local lamp = findEntity("Special Lamp")
+-- local area = findEntity("Some Area")
+-- local practiceArea = findEntity("practiceArea")
 
-local testSwitch = vars["testSwitch"]
+-- local testSwitch = vars["testSwitch"]
 
-local test_dialog = cutscene(player, function()
-    dialog("This is long text to make sure text wrap algorithm doesn't completely screw up the dialog box output.")
-    dialog("The gate here will shut!")
-    testSwitch.Value = false
-    instantiate("Goblin", 170, 116)
-end)
+-- local test_dialog = cutscene(player, function()
+--     dialog("This is long text to make sure text wrap algorithm doesn't completely screw up the dialog box output.")
+--     dialog("The gate here will shut!")
+--     testSwitch.Value = false
+--     instantiate("Goblin", 170, 116)
+-- end)
 
--- print("test debug log")
+-- -- print("test debug log")
 
--- print(roomVars["str"])
--- print(roomVars["int"])
--- roomVars["int"] = "abc"
--- print(roomVars["int"])
--- roomVars["new"] = 4.0
--- print(roomVars["new"])
+-- -- print(roomVars["str"])
+-- -- print(roomVars["int"])
+-- -- roomVars["int"] = "abc"
+-- -- print(roomVars["int"])
+-- -- roomVars["new"] = 4.0
+-- -- print(roomVars["new"])
 
--- print(roomVars["strVal"])
--- print(roomVars["someBool"])
-
-trigger(
-    onEnter(player, area),
-    test_dialog)
+-- -- print(roomVars["strVal"])
+-- -- print(roomVars["someBool"])
 
 -- trigger(
---     onEnter(player, practiceArea),
---     cutscene(player, function()
---         dialog("This text is now edited.")
---     end)
--- )
+--     onEnter(player, area),
+--     test_dialog)
+
+-- -- trigger(
+-- --     onEnter(player, practiceArea),
+-- --     cutscene(player, function()
+-- --         dialog("This text is now edited.")
+-- --     end)
+-- -- )
