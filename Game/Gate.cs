baebc334533cf3837e83d4cf312e99b678b0490b
Tanner Evins <tanner@tannerevins.com>
2021-02-03 07:16:32 -0600

    Use animations in Gate

--- Game/Gate.cs
+++ Game/Gate.cs
@@ -1,22 +1,24 @@
 ﻿using Game.Editor.Prefab;
 using Game.Editor.Scriptable;
 using Nez;
-using Nez.Sprites;
 using Nez.Textures;
+using static Game.Editor.Prefab.PlayerMovementData;
 
 namespace Game
 {
     [EntityOnly(typeof(GateState))]
     class GateData : DataComponent
     {
-        public TextureMapSpriteData ClosedSprite = new TextureMapSpriteData();
-        public TextureMapSpriteData OpenSprite = new TextureMapSpriteData();
+        public AnimationData OpenAnimation = new AnimationData();
+        public AnimationData CloseAnimation = new AnimationData();
 
         public override void AddToEntity(Entity entity)
         {
             var gate = entity.AddComponent<Gate>();
-            gate.ClosedSprite = ClosedSprite.Sprite;
-            gate.OpenSprite = OpenSprite.Sprite;
+            gate.OpenAnimation = OpenAnimation.MakeAnimation();
+            gate.CloseAnimation = CloseAnimation.MakeAnimation();
+            gate.AddComponent<Animator<ObserverFrame>>();
+            gate.AddComponent<SpriteObserver>();
         }
     }
 
@@ -33,23 +35,41 @@ namespace Game
 
     class Gate : Component, IUpdatable
     {
-        public Sprite ClosedSprite;
-        public Sprite OpenSprite;
+        public Animation<ObserverFrame> OpenAnimation;
+        public Animation<ObserverFrame> CloseAnimation;
         public BooleanValue OpenState;
 
         Collider _collider;
-        SpriteRenderer _renderer;
+        Animator<ObserverFrame> _animator;
+        bool _lastState;
 
         public override void OnAddedToEntity()
         {
             _collider = Entity.GetComponent<Collider>();
-            _renderer = Entity.GetComponent<SpriteRenderer>();
+            _animator = Entity.GetComponent<Animator<ObserverFrame>>();
+            _lastState = OpenState.RuntimeValue;
         }
 
         public void Update()
         {
-            _collider.Enabled = !OpenState.RuntimeValue;
-            _renderer.Sprite = OpenState.RuntimeValue ? OpenSprite : ClosedSprite;
+            if (OpenState.RuntimeValue && !_lastState)
+            {
+                _animator.Play(OpenAnimation, Animator<ObserverFrame>.LoopMode.ClampForever);
+            }
+            else if (!OpenState.RuntimeValue && _lastState)
+            {
+                _animator.Play(CloseAnimation, Animator<ObserverFrame>.LoopMode.ClampForever);
+            }
+
+            if (!_animator.IsRunning && OpenState.RuntimeValue)
+            {
+                _collider.Enabled = false;
+            }
+            else if (!OpenState.RuntimeValue)
+            {
+                _collider.Enabled = true;
+            }
+            _lastState = OpenState.RuntimeValue;
         }
     }
 }
