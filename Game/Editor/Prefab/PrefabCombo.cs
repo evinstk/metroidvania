c83db33c2f5e197457642cc74e9ff7da6aee2351
Tanner Evins <tanner@tannerevins.com>
2021-02-06 15:12:49 -0600

    Add prefab combo

---
+++ Game/Editor/Prefab/PrefabCombo.cs
@@ -0,0 +1,27 @@
+﻿using Nez;
+using Nez.ImGuiTools.TypeInspectors;
+
+namespace Game.Editor.Prefab
+{
+    [CustomInspector(typeof(PrefabComboInspector))]
+    class PrefabCombo
+    {
+        public string PrefabId;
+    }
+
+    class PrefabComboInspector : AbstractTypeInspector
+    {
+        PrefabManager _prefabManager;
+
+        public override void Initialize()
+        {
+            _prefabManager = Core.GetGlobalManager<PrefabManager>();
+        }
+
+        public override void DrawMutable()
+        {
+            var obj = GetValue<PrefabCombo>();
+            _prefabManager.Combo($"{_name}", ref obj.PrefabId);
+        }
+    }
+}
