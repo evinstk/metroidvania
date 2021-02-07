3a675380a930678e68ab601448a44c7f28f550da
Tanner Evins <tanner@tannerevins.com>
2021-02-07 07:30:00 -0600

    Add Spawner component

---
+++ Game/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using Game.Editor.Prefab;
+using Microsoft.Xna.Framework;
+using Nez;
+
+namespace Game
+{
+    class SpawnerData : DataComponent
+    {
+        public PrefabCombo Prefab = new PrefabCombo();
+        public Vector2 Offset;
+        public string EntityName = string.Empty;
+
+        public override void AddToEntity(Entity entity)
+        {
+            var spawner = entity.AddComponent<Spawner>();
+            spawner.Prefab = Core.GetGlobalManager<PrefabManager>().GetResource(Prefab.PrefabId);
+            spawner.Offset = Offset;
+            spawner.EntityName = EntityName;
+        }
+    }
+
+    class Spawner : Component
+    {
+        public PrefabData Prefab;
+        public Vector2 Offset;
+        public string EntityName;
+
+        public Entity Spawn(bool flip = false)
+        {
+            var entity = Prefab.CreateEntity(
+                string.IsNullOrEmpty(EntityName) ? Prefab.Name : EntityName,
+                Entity.Scene);
+            entity.Position = Entity.Position + new Vector2(flip ? -Offset.X : Offset.X, Offset.Y);
+            return entity;
+        }
+    }
+}
