d3a87117d98116c07c6396f14d7ac4224e585165
Tanner Evins <tanner@tannerevins.com>
2021-02-25 07:34:38 -0600

    Rotate projectile

--- Game/Projectile.cs
+++ Game/Projectile.cs
@@ -1,4 +1,5 @@
-﻿using Nez;
+﻿using Microsoft.Xna.Framework;
+using Nez;
 
 namespace Game
 {
@@ -9,12 +10,21 @@ namespace Game
         float _timer = 0;
         bool _pendingDestroy = false;
 
+        PlatformerMover _mover;
+
+        public override void OnAddedToEntity()
+        {
+            _mover = Entity.GetComponent<PlatformerMover>();
+        }
+
         public void Update()
         {
             _timer += Time.DeltaTime;
 
             if (_timer > TimeToLive || _pendingDestroy)
                 Entity.Destroy();
+
+            Transform.Rotation = Mathf.Deg2Rad * Mathf.RoundToNearest(Vector2.Zero.AngleBetween(new Vector2(1, 0), _mover.Speed), 45);
         }
 
         public void OnHurt(Hurtable self, Collider attacker)
