d6fd5bfc7efa2890270d5b73c58e76e27ef8eeff
Tanner Evins <tanner@tannerevins.com>
2021-02-28 06:48:37 -0600

    Give projectile buffer time to destroy itself

--- Game/Projectile.cs
+++ Game/Projectile.cs
@@ -7,9 +7,11 @@ namespace Game
     class Projectile : Component, IUpdatable
     {
         public float TimeToLive = 2f;
+        public int DestroyTickMax = 5;
 
         float _timer = 0;
         bool _pendingDestroy = false;
+        int _destroyTicks = 0;
 
         PlatformerMover _mover;
 
@@ -23,8 +25,12 @@ namespace Game
             _timer += Time.DeltaTime;
 
             if (_pendingDestroy)
+                ++_destroyTicks;
+
+            var shouldDestroy = _destroyTicks >= DestroyTickMax;
+            if (shouldDestroy)
                 Factory.CreateBoom(Entity.Scene, Entity.Position - new Vector2(Math.Sign(_mover.Speed.X), Math.Sign(_mover.Speed.Y)));
-            if (_timer > TimeToLive || _pendingDestroy)
+            if (_timer > TimeToLive || shouldDestroy)
                 Entity.Destroy();
 
             Transform.Rotation = Mathf.Deg2Rad * Mathf.RoundToNearest(Vector2.Zero.AngleBetween(new Vector2(1, 0), _mover.Speed), 45);
