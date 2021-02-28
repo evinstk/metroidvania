c9ac96a7f49468bd7b4a37ba33af88c7250b4443
Tanner Evins <tanner@tannerevins.com>
2021-02-28 17:33:06 -0600

    Remove commented out code

--- Game/Game.cs
+++ Game/Game.cs
@@ -39,27 +39,6 @@ namespace Game
             var systemResult = fmodSystem.initialize(8, FMOD.Studio.INITFLAGS.NORMAL, FMOD.INITFLAGS.NORMAL, IntPtr.Zero);
             Insist.IsTrue(systemResult == FMOD.RESULT.OK);
 
-            //var debugResult = FMOD.Debug.Initialize(FMOD.DEBUG_FLAGS.LOG, FMOD.DEBUG_MODE.FILE, null, "fmod_log.txt");
-            //Insist.IsTrue(debugResult == FMOD.RESULT.OK);
-
-            //var bankResult = fmodSystem.loadBankFile($"{ContentPath.FMOD}Master.bank", FMOD.Studio.LOAD_BANK_FLAGS.NORMAL, out var stuff);
-            //Insist.IsTrue(bankResult == FMOD.RESULT.OK);
-            //bankResult = fmodSystem.loadBankFile($"{ContentPath.FMOD}Master.strings.bank", FMOD.Studio.LOAD_BANK_FLAGS.NORMAL, out var stringBank);
-            //Insist.IsTrue(bankResult == FMOD.RESULT.OK);
-
-            //core.LoadBank("Master");
-            //core.LoadBank("Master.strings");
-
-            //bankResult = fmodSystem.loadBankFile($"{ContentPath.FMOD}Common.bank", FMOD.Studio.LOAD_BANK_FLAGS.NORMAL, out var bank);
-            //Insist.IsTrue(bankResult == FMOD.RESULT.OK);
-
-            //bank.loadSampleData();
-            //bank.getEventList(out var events);
-            //events[1].createInstance(out var instance);
-            ////instance.setParameterByName("RPM", 500);
-            //var startResult = instance.start();
-            //Insist.IsTrue(startResult == FMOD.RESULT.OK);
-
             return fmodSystem;
         }
 
