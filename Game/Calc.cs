f9019344ee62812f526f487d0e8b76605d241709
Tanner Evins <tanner@tannerevins.com>
2021-03-03 07:59:42 -0600

    Add Calc class

---
+++ Game/Calc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Game
+{
+    static class Calc
+    {
+        static Random rnd = new Random();
+
+        public static int RandInt(int maxExc)
+        {
+            if (maxExc <= 0)
+                return 0;
+            return rnd.Next(0, maxExc);
+        }
+
+        public static int RandInt(int min, int maxExc)
+        {
+            return rnd.Next(min, maxExc);
+        }
+    }
+}
