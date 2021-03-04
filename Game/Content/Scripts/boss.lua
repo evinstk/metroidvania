bde5e6d4b27cc4b39c38da5e5b0e95067238f5b7
Tanner Evins <tanner@tannerevins.com>
2021-03-03 18:58:15 -0600

    Update content

---
+++ Game/Content/Scripts/boss.lua
@@ -0,0 +1,22 @@
+﻿local boss = find_entity('boss')
+
+-- local boss_pos = boss.get_position()
+-- print(boss_pos.x, boss_pos.y)
+
+-- local player = find_entity('player')
+
+-- start_coroutine(function()
+--     wait(function()
+--         return player.in_area('enter')
+--     end)
+--     line('Boss time!')
+--     interact()
+--     line()
+-- end)
+
+start_coroutine(function()
+    wait(function()
+        return boss.get_health() <= 0
+    end)
+    vars['boss_switch'] = true
+end)
