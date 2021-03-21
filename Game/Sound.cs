52deeb81a8ec0869887d6664b8c0c10af6cb65db
Tanner Evins <tanner@tannerevins.com>
2021-03-21 07:52:34 -0500

    Add Sound component

---
+++ Game/Sound.cs
@@ -0,0 +1,28 @@
+﻿using Nez;
+
+namespace Game
+{
+    class Sound : Component
+    {
+        public FMOD.Studio.EventInstance Event;
+
+        string _bankName;
+        string _evt;
+
+        public Sound(string bankName, string evt)
+        {
+            _bankName = bankName;
+            _evt = evt;
+        }
+
+        public override void OnAddedToEntity()
+        {
+            Event = GameContent.LoadSound(_bankName, _evt);
+        }
+
+        public override void OnRemovedFromEntity()
+        {
+            Event.release();
+        }
+    }
+}
