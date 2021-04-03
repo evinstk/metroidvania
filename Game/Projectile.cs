693e7329c3369ae7e5eb3fb7def331d280575448
Tanner Evins <tanner@tannerevins.com>
2021-04-03 10:31:15 -0500

    Destroy projectile on hurt

--- Game/Projectile.cs
+++ Game/Projectile.cs
@@ -10,7 +10,6 @@ namespace Game
         public int DestroyTickMax = 5;
 
         float _timer = 0;
-        bool _pendingDestroy = false;
 
         PlatformerMover _mover;
 
@@ -23,9 +22,7 @@ namespace Game
         {
             _timer += Time.DeltaTime;
 
-            if (_pendingDestroy)
-                Factory.CreateBoom(Entity.Scene, Entity.Position - new Vector2(Math.Sign(_mover.Speed.X), Math.Sign(_mover.Speed.Y)));
-            if (_timer > TimeToLive || _pendingDestroy)
+            if (_timer > TimeToLive)
                 Entity.Destroy();
 
             Transform.Rotation = Mathf.Deg2Rad * Mathf.RoundToNearest(Vector2.Zero.AngleBetween(new Vector2(1, 0), _mover.Speed), 22.5f);
@@ -35,7 +32,8 @@ namespace Game
 
         public void OnHurt(Hurtable self, Collider attacker)
         {
-            _pendingDestroy = true;
+            Factory.CreateBoom(Entity.Scene, Entity.Position - new Vector2(Math.Sign(_mover.Speed.X), Math.Sign(_mover.Speed.Y)));
+            Entity.Destroy();
         }
     }
 }
