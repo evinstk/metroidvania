a94f06f68e9c742ea9b7b1cec0f281527e2f6449
Tanner Evins <tanner@tannerevins.com>
2021-04-17 18:45:35 -0500

    Add keese entity

---
+++ Game/Entities/Factories/Keese.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Nez;
+using System.Collections.Generic;
+
+namespace Game.Entities.Factories
+{
+    [EntityDef("keese")]
+    class Keese : EntityFactory
+    {
+        static Dictionary<string, int> _fps = new Dictionary<string, int>
+        {
+            { "fly", 14 },
+        };
+
+        public override Entity Create(OgmoEntity ogmoEntity, Vector2 position)
+        {
+            var entity = Core.Scene.CreateEntity("keese", position);
+
+            var anim = entity.AddComponent(Animator.MakeAnimator("keese", Core.Scene.Content, _fps));
+            anim.Play("fly");
+            anim.RenderLayer = -5;
+
+            var collider = entity.AddComponent(new BoxCollider(16, 16));
+            collider.PhysicsLayer = Mask.Enemy;
+
+            return entity;
+        }
+    }
+}
