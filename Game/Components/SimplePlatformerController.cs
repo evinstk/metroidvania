ee56d536c7b0db16321925397c91ce93bac295bd
Tanner Evins <tanner@tannerevins.com>
2021-04-17 09:24:55 -0500

    Implement SimplePlayer

---
+++ Game/Components/SimplePlatformerController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Xna.Framework.Input;
+using Nez;
+
+namespace Game.Components
+{
+    class SimplePlatformerController : Component, IUpdatable
+    {
+        public float Gravity = 600f;
+        public float MoveSpeed = 150f;
+        public float JumpTime = .4f;
+        public float JumpSpeed = 200f;
+
+        PlatformerMover _mover;
+
+        VirtualIntegerAxis _inputX;
+        VirtualButton _inputJump;
+
+        float _jumpTimer;
+
+        public override void OnAddedToEntity()
+        {
+            _mover = Entity.GetComponent<PlatformerMover>();
+
+            _inputX = new VirtualIntegerAxis();
+            _inputX.AddGamePadLeftStickX();
+            _inputX.AddKeyboardKeys(VirtualInput.OverlapBehavior.CancelOut, Keys.A, Keys.D);
+            _inputX.AddKeyboardKeys(VirtualInput.OverlapBehavior.CancelOut, Keys.Left, Keys.Right);
+
+            _inputJump = new VirtualButton();
+            _inputJump.AddGamePadButton(0, Buttons.A);
+            _inputJump.AddKeyboardKey(Keys.Space);
+        }
+
+        public void Update()
+        {
+            var onGround = _mover.OnGround();
+            var inputX = _inputX.Value;
+
+            // horizontal movement
+            {
+                _mover.Speed.X = inputX * MoveSpeed;
+            }
+
+            // invoke jumping
+            {
+                if (_inputJump.IsPressed && onGround)
+                {
+                    _jumpTimer = JumpTime;
+                }
+            }
+
+            // gravity
+            if (!onGround)
+            {
+                _mover.Speed.Y += Gravity * Time.DeltaTime;
+            }
+
+            // jumping
+            if (_jumpTimer > 0)
+            {
+                _jumpTimer -= Time.DeltaTime;
+                _mover.Speed.Y = -JumpSpeed;
+                if (!_inputJump.IsDown || _mover.OnGround(-1) || _jumpTimer <= 0)
+                {
+                    _jumpTimer = 0;
+                    _mover.Speed.Y = 0;
+                }
+            }
+        }
+    }
+}
--- Game/Entities/Factories/SimplePlayer.cs
+++ Game/Entities/Factories/SimplePlayer.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Game.Components;
+using Microsoft.Xna.Framework;
 using Nez;
+using System;
 
 namespace Game.Entities.Factories
 {
@@ -12,6 +14,26 @@ namespace Game.Entities.Factories
 
             var renderer = entity.AddComponent(new PrototypeSpriteRenderer(16, 16));
             renderer.Color = Color.Red;
+
+            var collider = entity.AddComponent(new BoxCollider(16, 16));
+            collider.PhysicsLayer = 0;
+
+            var mover = entity.AddComponent<PlatformerMover>();
+            mover.Collider = collider;
+
+            var controller = entity.AddComponent<SimplePlatformerController>();
+            var values = ogmoEntity.values;
+            if (values != null)
+            {
+                if (values.ContainsKey("gravity"))
+                    controller.Gravity = Convert.ToSingle(values["gravity"]);
+                if (values.ContainsKey("move_speed"))
+                    controller.MoveSpeed = Convert.ToSingle(values["move_speed"]);
+                if (values.ContainsKey("jump_time"))
+                    controller.JumpTime = Convert.ToSingle(values["jump_time"]);
+                if (values.ContainsKey("jump_speed"))
+                    controller.JumpSpeed = Convert.ToSingle(values["jump_speed"]);
+            }
 
             return entity;
         }
