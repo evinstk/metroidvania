7e637a0007d246d505b27454a8a4c1d7c80c5aa6
Tanner Evins <tanner@tannerevins.com>
2021-04-17 18:45:04 -0500

    Set simple_player RenderLayer

--- Game/Entities/Factories/SimplePlayer.cs
+++ Game/Entities/Factories/SimplePlayer.cs
@@ -14,6 +14,7 @@ namespace Game.Entities.Factories
 
             var renderer = entity.AddComponent(new PrototypeSpriteRenderer(16, 16));
             renderer.Color = Color.Red;
+            renderer.RenderLayer = -10;
 
             var collider = entity.AddComponent(new BoxCollider(16, 16));
             collider.PhysicsLayer = 0;
