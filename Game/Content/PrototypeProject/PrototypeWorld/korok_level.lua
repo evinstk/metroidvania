ca9f851f3d14aaf9e039fff201e86568a771a3c7
Tanner Evins <tanner@tannerevins.com>
2021-04-18 10:03:17 -0500

    Update prototype level

---
+++ Game/Content/PrototypeProject/PrototypeWorld/korok_level.lua
@@ -0,0 +1,11 @@
+﻿print('playing korok_level')
+
+on_event('found_korok_seed', function(korok_entity)
+    print(korok_entity.get_name())
+end)
+
+-- start_coroutine(function()
+--     while true do
+--         coroutine.yield()
+--     end
+-- end)
