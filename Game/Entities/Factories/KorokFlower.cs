0898af680e30d2f444b7ff7f166f20b5c2da2a72
Tanner Evins <tanner@tannerevins.com>
2021-04-18 10:02:52 -0500

    Implement korok_flower

--- Game/Entities/Factories/KorokFlower.cs
+++ Game/Entities/Factories/KorokFlower.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Game.Components;
+using Game.Scripts;
+using Microsoft.Xna.Framework;
 using Nez;
 using System.Collections.Generic;
 
@@ -10,15 +12,46 @@ namespace Game.Entities.Factories
         static Dictionary<string, int> _fps = new Dictionary<string, int>
         {
             { "korok_flower_idle", 4 },
+            { "korok_flower_final_idle", 4 },
         };
 
         public override Entity Create(OgmoEntity ogmoEntity, Vector2 position)
         {
+            var nodes = new List<Vector2>();
+            foreach (var node in ogmoEntity.nodes)
+                nodes.Add(node);
+            return Instantiate(position, nodes);
+        }
+
+        Entity Instantiate(Vector2 position, List<Vector2> nodes)
+        {
             var entity = Core.Scene.CreateEntity("korok_flower", position);
 
             var anim = entity.AddComponent(Animator.MakeAnimator("prototype", Core.Scene.Content, _fps));
-            anim.Play("korok_flower_idle");
+            anim.Play(nodes.Count > 0 ? "korok_flower_idle" : "korok_flower_final_idle");
             anim.RenderLayer = -5;
+
+            var collider = entity.AddComponent<BoxCollider>();
+            collider.PhysicsLayer = 0;
+            collider.CollidesWithLayers = Mask.Player;
+
+            entity.AddComponent(new Trigger(
+                self => self.GetComponent<BoxCollider>().CollidesWithAny(out _),
+                self =>
+                {
+                    if (nodes.Count > 0)
+                    {
+                        var next = nodes[0];
+                        nodes.RemoveAt(0);
+                        Instantiate(next, nodes);
+                        self.Entity.Destroy();
+                    }
+                    else
+                    {
+                        Core.Scene.GetSceneComponent<ScriptLoader>().RaiseEvent("found_korok_seed", self.Entity);
+                        self.Entity.Destroy();
+                    }
+                }));
 
             return entity;
         }
