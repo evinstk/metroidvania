e9ab8a807542530264fabac02a614d48b4c3836f
Tanner Evins <tanner@tannerevins.com>
2021-04-18 21:27:05 -0500

    Add korok_platform

--- Game/Content/PrototypeProject/PrototypeWorld/korok_level.lua
+++ Game/Content/PrototypeProject/PrototypeWorld/korok_level.lua
@@ -3,10 +3,14 @@
 -- local korok_sound = core.load_sound('FMOD/Common.bank', 'save')
 local korok_sound = audio.load_sound('FMOD/Common.bank', 'korok_discovery')
 
-on_event('found_korok_seed', function(korok_entity)
-    local pos = korok_entity.get_position()
+on_event('found_korok_seed', function(flower)
+    local pos = flower.get_position()
     scene.instantiate('korok', pos)
     korok_sound.start()
+end)
+
+on_event('korok_platform_hit', function(platform)
+    print(platform.get_name())
 end)
 
 -- start_coroutine(function()
---
+++ Game/Entities/Factories/KorokPlatform.cs
@@ -0,0 +1,39 @@
+﻿using Game.Scripts;
+using Microsoft.Xna.Framework;
+using Nez;
+using System.Collections.Generic;
+
+namespace Game.Entities.Factories
+{
+    [EntityDef("korok_platform")]
+    class KorokPlatform : EntityFactory
+    {
+        Dictionary<string, int> _fps = new Dictionary<string, int>
+        {
+            { "korok_platform_active", 15 },
+        };
+
+        public override Entity Create(OgmoEntity ogmoEntity, Vector2 position)
+        {
+            var entity = Core.Scene.CreateEntity("korok_platform", position);
+
+            var anim = entity.AddComponent(Animator.MakeAnimator("prototype", Core.Scene.Content, _fps));
+            anim.Play("korok_platform_active");
+            anim.RenderLayer = -15;
+
+            var collider = entity.AddComponent(new BoxCollider(-14, 12, 28, 4));
+            collider.PhysicsLayer = Mask.Terrain;
+
+            var triggerArea = entity.AddComponent(new BoxCollider(-1, 11, 2, 1));
+            triggerArea.PhysicsLayer = 0;
+            triggerArea.CollidesWithLayers = Mask.Player;
+
+            entity.AddComponent(new Trigger(
+                self => triggerArea.CollidesWithAny(out _),
+                self => Core.Scene.GetSceneComponent<ScriptLoader>().RaiseEvent("korok_platform_hit", self.Entity),
+                true));
+
+            return entity;
+        }
+    }
+}
