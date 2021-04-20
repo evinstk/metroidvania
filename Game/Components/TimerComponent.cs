6a84c3b36fd5d4442c465b41d81babf7986d08b5
Tanner Evins <tanner@tannerevins.com>
2021-04-20 17:42:07 -0500

    Add TimerComponent

---
+++ Game/Components/TimerComponent.cs
@@ -0,0 +1,34 @@
+﻿using Nez;
+using System;
+
+namespace Game.Components
+{
+    class TimerComponent : Component, IUpdatable
+    {
+        float _duration;
+        Action<TimerComponent> _callback;
+
+        public TimerComponent(float duration, Action<TimerComponent> callback)
+        {
+            _duration = duration;
+            _callback = callback;
+        }
+
+        void Start(float duration)
+        {
+            _duration = duration;
+        }
+
+        public void Update()
+        {
+            if (_duration > 0)
+            {
+                _duration -= Time.DeltaTime;
+                if (_duration <= 0 && _callback != null)
+                {
+                    _callback(this);
+                }
+            }
+        }
+    }
+}
