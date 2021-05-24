8dbe42227e9902f0541c991dd693f812839fc11a
Tanner Evins <tanner@tannerevins.com>
2021-05-24 06:41:02 -0700

    Remove Class1 construction

--- Game/Prototypes/PrototypeScene.cs
+++ Game/Prototypes/PrototypeScene.cs
@@ -24,7 +24,6 @@ namespace Game.Prototypes
 
         public override void OnStart()
         {
-            new Engine.Class1();
             AddSceneComponent<SceneSettings>();
             AddSceneComponent<EntityLoader>();
             AddSceneComponent<WorldLoader>();
